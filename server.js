ea35678d1ecb684ef0316cca0ad9ffe12f82e2ed
Anthony Pena <apena5@alumni.jh.edu>
2021-09-15 04:57:43 -0400

    connected server

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.synch({ force: false }).then(() => {
+
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
 });
 
+console.log('here');
